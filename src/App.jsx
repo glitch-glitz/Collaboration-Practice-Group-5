8f7752140534c5ccf2208dfe9dcc8e90b2b4eace
glitch-glitz <fullstack.hub.devx@gmail.com>
2025-04-17 21:26:40 +0300

    Revert " PricePerNight component to display nightly prices"

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,16 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-
-import PricePerNight from "./components/PricePerNight";
-
 import { HotelNames } from "./Components/HotelNames";
-
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-      <PricePerNight />
       <HotelNames />
-</div>
     </>
   );
 }
+
 export default App;
